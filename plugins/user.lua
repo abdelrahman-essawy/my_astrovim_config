db2b6a7d5a443dbbe16ac4f9d9b6a591bee2616c
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-08-20 08:47:38 +0300

    init

---
+++ plugins/user.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function()
+      require("lsp_signature").setup()
+    end,
+  },
+  {
+    "andweeb/presence.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "github/copilot.vim",
+    event = "VeryLazy",
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_tab_fallback = ""
+      vim.g.copilot_filetypes = {
+        ["*"] = false,
+        python = true,
+        lua = true,
+        go = true,
+        rust = true,
+        html = true,
+        c = true,
+        cpp = true,
+        java = true,
+        javascript = true,
+        typescript = true,
+        javascriptreact = true,
+        typescriptreact = true,
+        terraform = true,
+      }
+    end,
+  },
+  {
+    "mg979/vim-visual-multi",
+    event = "VeryLazy",
+  },
+}
